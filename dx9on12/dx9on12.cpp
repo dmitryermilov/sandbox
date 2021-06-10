95c7e0eee488a626a5d6512d0f1630bed6704194
Dmitry Ermilov <dmitry.ermilov@intel.com>
2021-06-10 20:16:18 +0300

    LockRect
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- dx9on12/dx9on12.cpp
+++ dx9on12/dx9on12.cpp
@@ -2,7 +2,6 @@
 #include "pch.h"
 #include "d3d11.h"
 #include "d3d11_1.h"
-#include "d3d12.h"
 #include "dxgi.h"
 #include "d3d9on12.h"
 #include "dxva.h"
@@ -12,6 +11,7 @@
 #include < tuple >
 #include < comdef.h >
 #include <string>
+#include <thread>
 
 struct GetMonitorRect_data {
 	int current;
@@ -84,25 +84,27 @@ CComPtr<IDirect3DDeviceManager9> deviceManager;
 CComPtr<IDirectXVideoProcessorService> service;
 CComPtr<IDirect3DDevice9Ex> d3d9Device;
 
-std::vector<IDirect3DSurface9*> d3d9Surfaces(2);
+std::vector<IDirect3DSurface9*> d3d9Surfaces(10);
 
-CComPtr<IDirect3DDevice9On12> d3dOn12;
-CComPtr<ID3D12Device>         d3d12;
-CComPtr<ID3D12CommandAllocator>     commandAllocator;
-CComPtr<ID3D12GraphicsCommandList>  commandList;
-CComPtr<ID3D12CommandQueue>         queue;
-CComPtr<ID3D12Fence>                fence;
-UINT64                              signalValue = 1;
-HANDLE                              hFenceEvent;
+int init(bool used3d9on12) {
 
-int init() {
-	D3D9ON12_ARGS args = {};
-	args.Enable9On12 = true;
+	HRESULT hr = 0;
+	if (used3d9on12)
+	{
+		printf("d3d9on12 path\n");
+		D3D9ON12_ARGS args = {};
+		args.Enable9On12 = true;
 
-	HRESULT hr = Direct3DCreate9On12Ex(D3D_SDK_VERSION, &args, 1, &d3d9);
-	if (FAILED(hr)) {
-		std::cout << "Direct3DCreate9On12 failed" << hr << std::endl;
-		return 1;
+		hr = Direct3DCreate9On12Ex(D3D_SDK_VERSION, &args, 1, &d3d9);
+		if (FAILED(hr)) {
+			std::cout << "Direct3DCreate9On12 failed" << hr << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		printf("d3d9 path\n");
+		hr = Direct3DCreate9Ex(D3D_SDK_VERSION, &d3d9);
 	}
 
 	GetMonitorRect_data monitor = { 0 };
@@ -158,7 +160,7 @@ int init() {
 	hr = service->CreateSurface(
 		4096,
 		2048,
-		2 - 1,
+		10 - 1,
 		(D3DFORMAT)MAKEFOURCC('N', 'V', '1', '2'),
 		//D3DFMT_A8R8G8B8,
 		D3DPOOL_DEFAULT,
@@ -171,191 +173,33 @@ int init() {
 		return 1;
 	}
 
-	hr = d3d9Device->QueryInterface(IID_PPV_ARGS(&d3dOn12));
-	if (FAILED(hr)) {
-		std::cout << "QueryInterface failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3dOn12->GetD3D12Device(IID_PPV_ARGS(&d3d12));
-	if (FAILED(hr)) {
-		std::cout << "GetD3D12Device failed " << hr << std::endl;
-		return 1;
-	}
-
-	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COPY;  //Specifies a command buffer for copying.
-	queueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL; //	Global realtime priority.
-	queueDesc.NodeMask = 0; //For single GPU operation, set this to zero.
-	hr = d3d12->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&queue));
-	if (FAILED(hr)) {
-		std::cout << "CreateCommandQueue failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3d12->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_COPY, IID_PPV_ARGS(&commandAllocator));
-	if (FAILED(hr)) {
-		std::cout << "CreateCommandAllocator failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3d12->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
-	if (FAILED(hr)) {
-		std::cout << "CreateFence failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3d12->CreateCommandList(
-		0,
-		D3D12_COMMAND_LIST_TYPE_COPY,
-		commandAllocator,
-		nullptr,
-		IID_PPV_ARGS(&commandList));
-	if (FAILED(hr)) {
-		std::cout << "CreateCommandList failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = commandList->Close();
-	if (FAILED(hr)) {
-		std::cout << "commandList::Close failed " << hr << std::endl;
-		return 1;
-	}
-
-	hFenceEvent = CreateEvent(nullptr, false, false, nullptr);
-	if (!hFenceEvent) {
-		std::cout << "CreateEvent failed" << std::endl;
-		return 1;
-	}
-	return 0;
-}
-
-int copy(IDirect3DSurface9* d3d9Dst, IDirect3DSurface9* d3d9Src) {
-	std::cout << "going to copy " << d3d9Dst << " to " << d3d9Src  << std::endl;
-	CComPtr <ID3D12Resource>  d3d12Src;
-	HRESULT hr = d3dOn12->UnwrapUnderlyingResource(d3d9Src, queue, IID_PPV_ARGS(&d3d12Src));
-	if (FAILED(hr)) {
-		std::cout << "UnwrapUnderlyingResource failed " << hr << std::endl;
-		return 1;
-	}
-
-	CComPtr <ID3D12Resource>  d3d12Dst;
-	hr = d3dOn12->UnwrapUnderlyingResource(d3d9Dst, queue, IID_PPV_ARGS(&d3d12Dst));
-	if (FAILED(hr)) {
-		std::cout << "UnwrapUnderlyingResource failed " << hr << std::endl;
-		return 1;
-	}
-
-	ID3D12Pageable* p[] = { d3d12Dst, d3d12Src };
-	hr = d3d12->MakeResident(2, p);
-	if (FAILED(hr)) {
-		std::cout << "MakeResident failed " << hr << std::endl;
-		return 1;
-	}
-
-	auto sdesc = d3d12Src->GetDesc();
-	auto ddesc = d3d12Dst->GetDesc();
-
-	hr = commandAllocator->Reset();
-	if (FAILED(hr)) {
-		std::cout << "m_commandAllocator->Reset failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = commandList->Reset(commandAllocator, nullptr);
-	if (FAILED(hr)) {
-		std::cout << "commandList->Reset failed " << hr << std::endl;
-		return 1;
-	}
-#if 0
-	commandList->CopyResource(d3d12Dst, d3d12Src);
-	if (FAILED(hr)) {
-		std::cout << "commandList->CopyResource failed " << hr << std::endl;
-		return 1;
-	}
-#else
-	D3D12_TEXTURE_COPY_LOCATION srcLocation{};
-	D3D12_TEXTURE_COPY_LOCATION dstLocation{};
-
-	srcLocation.pResource = d3d12Src;
-	srcLocation.SubresourceIndex = 0;
-	srcLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-
-	dstLocation.pResource = d3d12Dst;
-	dstLocation.SubresourceIndex = 1;
-	dstLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-
-	commandList->CopyTextureRegion(&dstLocation, 0, 0, 0, &srcLocation, nullptr);
-
-#endif
-
-	hr = commandList->Close();
-	if (FAILED(hr)) {
-		std::cout << "commandList->Close failed " << hr << std::endl;
-		return 1;
-	}
-
-	std::vector<ID3D12CommandList*> cmdLists = { commandList };
-	queue->ExecuteCommandLists((UINT)cmdLists.size(), cmdLists.data());
-
-	hr = queue->Signal(fence, signalValue);
-	if (FAILED(hr)) {
-		std::cout << "queue->Signal failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = fence->SetEventOnCompletion(signalValue, hFenceEvent);
-	if (FAILED(hr)) {
-		std::cout << "fence->SetEventOnCompletion failed " << hr << std::endl;
-		return 1;
-	}
-
-	DWORD dw = WaitForSingleObject(hFenceEvent, INFINITE);
-	std::cout << "WaitForSingleObject returned " << dw << std::endl;
-	++signalValue;
-
-	ID3D12Fence* ar1[] = { fence };
-	UINT64 ar0[] = { signalValue };
-	hr = d3dOn12->ReturnUnderlyingResource(d3d9Src, 1, ar0, ar1);
-	if (FAILED(hr)) {
-		std::cout << "d3dOn12->ReturnUnderlyingResource failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3dOn12->ReturnUnderlyingResource(d3d9Dst, 1, ar0, ar1);
-	if (FAILED(hr)) {
-		std::cout << "d3dOn12->ReturnUnderlyingResource failed " << hr << std::endl;
-		return 1;
-	}
-
-	hr = d3d9Device->CheckDeviceState(nullptr);
-	if (FAILED(hr)) {
-		_com_error err(hr);
-		LPCTSTR errMsg = err.ErrorMessage();
-		std::wstring  s(errMsg);
-		std::wcout << "d3d9Device->CheckDeviceState failed(" <<  hr << ") "<< s << std::endl;
-		
-		return 1;
-	}
-
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	CComPtr<ID3D12Debug> debugController;
-	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-	    debugController->EnableDebugLayer();
 
-	if (init()) {
+	bool used3d9on12 = !!(argc > 1);
+
+	if (init(used3d9on12)) {
 		std::cout << "Init failed " << std::endl;
 		return -1;
 	}
-	for (int i = 0; i < 10; ++i) {
-		if (copy(d3d9Surfaces[0], d3d9Surfaces[1])) {
-			std::cout << "Copy failed at iteration " << i << std::endl;
-			return -1;
+
+	for (size_t i = 0; i < 2000;++i)
+	{
+		D3DLOCKED_RECT rect;
+		auto hr = d3d9Surfaces[0]->LockRect(&rect, NULL, D3DLOCK_NOSYSLOCK);
+		if (FAILED(hr)) {
+			printf("LockRect FAILED\n");
+			abort();
+		}
+		//char* data = (char*)rect.pBits;
+		//data = 0x0;
+		hr = d3d9Surfaces[0]->UnlockRect();
+		if (FAILED(hr)) {
+			printf("UnlockRect FAILED\n");
+			abort();
 		}
 	}
 
