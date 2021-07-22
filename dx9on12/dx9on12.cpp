e573297eb587a375aeb127435c45260dfd860d9c
Dmitry Ermilov <dmitry.ermilov@intel.com>
2021-07-22 15:35:03 +0300

    AcquireSync/ReleaseSync
    Signed-off-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- dx9on12/dx9on12.cpp
+++ dx9on12/dx9on12.cpp
@@ -3,377 +3,233 @@
 #include "d3d9on12.h"
 #include "dxva.h"
 #include "dxva2api.h"
+#include "d3d11.h"
 #include <iostream>
 #include <vector>
 #include <comdef.h>
 #include <string>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <dxgi1_2.h>
+#include <d3d11_1.h>
+#include <d3d11_4.h>
 
-struct GetMonitorRect_data {
-    int current;
-    int required;
-    RECT requiredRect;
-};
-BOOL CALLBACK GetMonitorRect_MonitorEnumProc(HMONITOR /*hMonitor*/,
-    HDC /*hdcMonitor*/,
-    LPRECT lprcMonitor,
-    LPARAM dwData)
+template <typename T>
+static inline T mfx_print_err(T sts, const char* file, int line, const char* func)
 {
-    GetMonitorRect_data* data = reinterpret_cast<GetMonitorRect_data*>(dwData);
-    RECT r = { 0 };
-    if (NULL == lprcMonitor)
-        lprcMonitor = &r;
-
-    if (data->current == data->required)
-        data->requiredRect = *lprcMonitor;
-    data->current++;
-
-    return TRUE;
-}
-
-class DeviceHandle
-{
-public:
-    DeviceHandle(IDirect3DDeviceManager9* manager)
-        : m_manager(manager)
-        , m_handle(0)
+    if (sts < 0)
     {
-        if (manager)
-        {
-            HRESULT hr = manager->OpenDeviceHandle(&m_handle);
-            if (FAILED(hr))
-                m_manager = 0;
-        }
-    }
-
-    ~DeviceHandle()
-    {
-        if (m_manager && m_handle)
-            m_manager->CloseDeviceHandle(m_handle);
-    }
-
-    HANDLE Detach()
-    {
-        HANDLE tmp = m_handle;
-        m_manager = 0;
-        m_handle = 0;
-        return tmp;
-    }
-
-    operator HANDLE()
-    {
-        return m_handle;
-    }
-
-    bool operator !() const
-    {
-        return m_handle == 0;
-    }
-
-protected:
-    CComPtr<IDirect3DDeviceManager9> m_manager;
-    HANDLE m_handle;
-};
-
-CComPtr<IDirect3D9Ex> d3d9;
-CComPtr<IDirect3DDeviceManager9> deviceManager;
-CComPtr<IDirectXVideoProcessorService> service;
-CComPtr<IDirect3DDevice9Ex> d3d9Device;
-
-CComPtr<IDirect3DDevice9On12> d3dOn12;
-CComPtr<ID3D12Device>         d3d12;
-CComPtr<ID3D12CommandAllocator>     commandAllocator;
-CComPtr<ID3D12GraphicsCommandList>  commandList;
-CComPtr<ID3D12CommandQueue>         queue;
-CComPtr<ID3D12Fence>                fence;
-UINT64                              signalValue = 1;
-HANDLE                              hFenceEvent;
-
-int initDX9Device() {
-    D3D9ON12_ARGS args = {};
-    args.Enable9On12 = true;
-
-    HRESULT hr = Direct3DCreate9On12Ex(D3D_SDK_VERSION, &args, 1, &d3d9);
-    if (FAILED(hr)) {
-        std::cout << "Direct3DCreate9On12 failed" << hr << std::endl;
-        return 1;
-    }
-
-    GetMonitorRect_data monitor = { 0 };
-    monitor.required = 0;
-    EnumDisplayMonitors(NULL, NULL, &GetMonitorRect_MonitorEnumProc, (LPARAM)&monitor);
-
-    POINT point = { monitor.requiredRect.left, monitor.requiredRect.top };
-    HWND  hWindow = WindowFromPoint(point);
-
-    D3DPRESENT_PARAMETERS d3dPar = {};
-    d3dPar.BackBufferFormat = (D3DFORMAT)22;
-    d3dPar.BackBufferCount = 24;
-    d3dPar.SwapEffect = D3DSWAPEFFECT_DISCARD;
-    d3dPar.hDeviceWindow = (HWND)hWindow;
-    d3dPar.Windowed = 1;
-    d3dPar.Flags = D3DPRESENTFLAG_VIDEO;
-    d3dPar.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT | D3DPRESENT_DONOTWAIT;
-
-    hr = d3d9->CreateDeviceEx(0,
-        D3DDEVTYPE_HAL,
-        (HWND)hWindow,
-        D3DCREATE_HARDWARE_VERTEXPROCESSING |
-        D3DCREATE_FPU_PRESERVE |
-        D3DCREATE_MULTITHREADED,
-        &d3dPar,
-        nullptr,
-        &d3d9Device);
-    if (FAILED(hr)) {
-        std::cout << "CreateDeviceEx failed " << hr << std::endl;
-        return 1;
-    }
-
-    UINT resetToken = 0;
-    hr = DXVA2CreateDirect3DDeviceManager9(&resetToken, &deviceManager);
-    if (FAILED(hr)) {
-        std::cout << "DXVA2CreateDirect3DDeviceManager9 failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = deviceManager->ResetDevice(d3d9Device, resetToken);
-    if (FAILED(hr)) {
-        std::cout << "deviceManager->ResetDevice failed " << hr << std::endl;
-        return 1;
-    }
-
-    DeviceHandle device = DeviceHandle(deviceManager);
-    hr = deviceManager->GetVideoService(device, IID_PPV_ARGS(&service));
-    if (FAILED(hr)) {
-        std::cout << "deviceManager->GetVideoService failed " << hr << std::endl;
-        return 1;
-    }
-
-    return 0;
-}
-
-int initDX12() {
-
-    auto hr = d3d9Device->QueryInterface(IID_PPV_ARGS(&d3dOn12));
-    if (FAILED(hr)) {
-        std::cout << "QueryInterface failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3dOn12->GetD3D12Device(IID_PPV_ARGS(&d3d12));
-    if (FAILED(hr)) {
-        std::cout << "GetD3D12Device failed " << hr << std::endl;
-        return 1;
-    }
-
-    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_COPY;  //Specifies a command buffer for copying.
-    queueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL; //    Global realtime priority.
-    queueDesc.NodeMask = 0; //For single GPU operation, set this to zero.
-    hr = d3d12->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&queue));
-    if (FAILED(hr)) {
-        std::cout << "CreateCommandQueue failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3d12->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_COPY, IID_PPV_ARGS(&commandAllocator));
-    if (FAILED(hr)) {
-        std::cout << "CreateCommandAllocator failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3d12->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
-    if (FAILED(hr)) {
-        std::cout << "CreateFence failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3d12->CreateCommandList(
-        0,
-        D3D12_COMMAND_LIST_TYPE_COPY,
-        commandAllocator,
-        nullptr,
-        IID_PPV_ARGS(&commandList));
-    if (FAILED(hr)) {
-        std::cout << "CreateCommandList failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = commandList->Close();
-    if (FAILED(hr)) {
-        std::cout << "commandList::Close failed " << hr << std::endl;
-        return 1;
-    }
-
-    hFenceEvent = CreateEvent(nullptr, false, false, nullptr);
-    if (!hFenceEvent) {
-        std::cout << "CreateEvent failed" << std::endl;
-        return 1;
-    }
-    return 0;
-}
-
-int copy(IDirect3DSurface9* d3d9Dst, IDirect3DSurface9* d3d9Src) {
-    std::cout << "going to copy " << d3d9Dst << " to " << d3d9Src  << std::endl;
-    CComPtr <ID3D12Resource>  d3d12Src;
-    HRESULT hr = d3dOn12->UnwrapUnderlyingResource(d3d9Src, queue, IID_PPV_ARGS(&d3d12Src));
-    if (FAILED(hr)) {
-        std::cout << "UnwrapUnderlyingResource failed " << hr << std::endl;
-        return 1;
-    }
-
-    CComPtr <ID3D12Resource>  d3d12Dst;
-    hr = d3dOn12->UnwrapUnderlyingResource(d3d9Dst, queue, IID_PPV_ARGS(&d3d12Dst));
-    if (FAILED(hr)) {
-        std::cout << "UnwrapUnderlyingResource failed " << hr << std::endl;
-        return 1;
-    }
-
-    ID3D12Pageable* p[] = { d3d12Dst, d3d12Src };
-    hr = d3d12->MakeResident(2, p);
-    if (FAILED(hr)) {
-        std::cout << "MakeResident failed " << hr << std::endl;
-        return 1;
-    }
-
-    auto sdesc = d3d12Src->GetDesc();
-    auto ddesc = d3d12Dst->GetDesc();
-
-    hr = commandAllocator->Reset();
-    if (FAILED(hr)) {
-        std::cout << "m_commandAllocator->Reset failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = commandList->Reset(commandAllocator, nullptr);
-    if (FAILED(hr)) {
-        std::cout << "commandList->Reset failed " << hr << std::endl;
-        return 1;
-    }
-#if 0
-    commandList->CopyResource(d3d12Dst, d3d12Src);
-    if (FAILED(hr)) {
-        std::cout << "commandList->CopyResource failed " << hr << std::endl;
-        return 1;
-    }
-#else
-    D3D12_TEXTURE_COPY_LOCATION srcLocation{};
-    D3D12_TEXTURE_COPY_LOCATION dstLocation{};
-
-    srcLocation.pResource = d3d12Src;
-    srcLocation.SubresourceIndex = 0;
-    srcLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-
-    dstLocation.pResource = d3d12Dst;
-    dstLocation.SubresourceIndex = 1;
-    dstLocation.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-
-    commandList->CopyTextureRegion(&dstLocation, 0, 0, 0, &srcLocation, nullptr);
-
-#endif
-
-    hr = commandList->Close();
-    if (FAILED(hr)) {
-        std::cout << "commandList->Close failed " << hr << std::endl;
-        return 1;
-    }
-
-    std::vector<ID3D12CommandList*> cmdLists = { commandList };
-    queue->ExecuteCommandLists((UINT)cmdLists.size(), cmdLists.data());
-
-    hr = queue->Signal(fence, signalValue);
-    if (FAILED(hr)) {
-        std::cout << "queue->Signal failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = fence->SetEventOnCompletion(signalValue, hFenceEvent);
-    if (FAILED(hr)) {
-        std::cout << "fence->SetEventOnCompletion failed " << hr << std::endl;
-        return 1;
-    }
-
-    DWORD dw = WaitForSingleObject(hFenceEvent, INFINITE);
-    std::cout << "WaitForSingleObject returned " << dw << std::endl;
-    ++signalValue;
-
-    ID3D12Fence* ar1[] = { fence };
-    UINT64 ar0[] = { signalValue };
-    hr = d3dOn12->ReturnUnderlyingResource(d3d9Src, 1, ar0, ar1);
-    if (FAILED(hr)) {
-        std::cout << "d3dOn12->ReturnUnderlyingResource failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3dOn12->ReturnUnderlyingResource(d3d9Dst, 1, ar0, ar1);
-    if (FAILED(hr)) {
-        std::cout << "d3dOn12->ReturnUnderlyingResource failed " << hr << std::endl;
-        return 1;
-    }
-
-    hr = d3d9Device->CheckDeviceState(nullptr);
-    if (FAILED(hr)) {
-        _com_error err(hr);
+        _com_error err(sts);
         LPCTSTR errMsg = err.ErrorMessage();
         std::wstring  s(errMsg);
-        std::wcout << "d3d9Device->CheckDeviceState failed(" <<  hr << ") "<< s << std::endl;
-        
-        return 1;
-    }
 
-    return 0;
+        std::wcout << "Failed at  line " << line << " with error " << sts << ") " << s << std::endl;
+    }
+    return sts;
+}
+#define MFX_STS_TRACE(sts) mfx_print_err(sts, __FILE__, __LINE__, __FUNCTION__)
+#define MFX_RETURN(sts)         { return MFX_STS_TRACE(sts); }
+
+#define MFX_CHECK(EXPR)    { if (FAILED(EXPR)) MFX_RETURN(EXPR); }
+
+std::chrono::system_clock::time_point start;
+
+int Routine(int idx, CComPtr<ID3D11Device> & device, CComPtr<ID3D11DeviceContext> & context, std::pair<CComPtr<ID3D11Texture2D>, UINT64> &texture)
+{
+    std::chrono::duration<double, std::milli> d = std::chrono::system_clock::now() - start;
+    printf("[%f]        Child session(%d) started\n", d.count(), idx );
+
+    CComPtr<IDXGIResource1> dxgiResource;
+    CComPtr<ID3D11Texture2D> sharedTexture;
+    MFX_CHECK(texture.first->QueryInterface(IID_PPV_ARGS(&dxgiResource)));
+
+    HANDLE h;
+    MFX_CHECK(dxgiResource->CreateSharedHandle(nullptr, DXGI_SHARED_RESOURCE_READ | DXGI_SHARED_RESOURCE_WRITE, nullptr, &h));
+    printf("[%f]        Child session(%d) called CreateSharedHandle\n", d.count(), idx);
+
+    CComPtr<ID3D11Device1> device1;
+    MFX_CHECK(device->QueryInterface(IID_PPV_ARGS(&device1)));
+    MFX_CHECK(device1->OpenSharedResource1(h, IID_PPV_ARGS(&sharedTexture)));
+    printf("[%f]        Child session(%d) called OpenSharedResource1\n", d.count(), idx);
+
+    CComPtr<IDXGIKeyedMutex> mutex;
+    MFX_CHECK(sharedTexture->QueryInterface(IID_PPV_ARGS(&mutex)));
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f]        Child session(%d) about to call AcquireSync(%llu)\n", d.count(), idx, texture.second);
+    MFX_CHECK(mutex->AcquireSync(texture.second, INFINITE));
+
+
+    D3D11_TEXTURE2D_DESC desc = { 0 };
+    texture.first->GetDesc(&desc);
+
+    desc.Width = desc.Width;
+    desc.Height = desc.Height;
+    desc.MipLevels = 1;
+    desc.Format = desc.Format;
+    desc.SampleDesc.Count = 1;
+    desc.ArraySize = 1;
+    desc.Usage = D3D11_USAGE_STAGING;
+    desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
+    desc.BindFlags = 0;
+    desc.MiscFlags = 0;
+
+    CComPtr<ID3D11Texture2D> staging;
+    MFX_CHECK(device->CreateTexture2D(&desc, NULL, &staging));
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f]       Child session(%d) started READ from surface \n", d.count(), idx);
+    context->CopySubresourceRegion(staging, 0, 0, 0, 0, sharedTexture, 0, NULL);
+    D3D11_MAPPED_SUBRESOURCE lockRect = {};
+    MFX_CHECK(context->Map(staging, 0, D3D11_MAP_READ, 0, &lockRect));
+    context->Unmap(staging, 0);
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f]       Child session(%d) finished READ from surface\n", d.count(), idx);
+
+    MFX_CHECK(mutex->ReleaseSync(texture.second));
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f]       Child session(%d) called ReleaseSync(%llu)\n", d.count(), idx, texture.second);
 }
 
 int main(int argc, char* argv[])
 {
-    CComPtr<ID3D12Debug> debugController;
-    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-        debugController->EnableDebugLayer();
 
-    if (initDX9Device()) {
-        std::cout << "initDX9Device failed " << std::endl;
-        return -1;
+    CComPtr<ID3D11Debug> debugController;
+    HRESULT hr = S_OK;
+    static D3D_FEATURE_LEVEL FeatureLevels[] = { D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0 };
+    D3D_FEATURE_LEVEL pFeatureLevelsOut;
+    CComPtr<ID3D11Device>         masterDevice;
+    CComPtr<ID3D11DeviceContext>  masterContext;
+
+    hr = D3D11CreateDevice(nullptr,    // provide real adapter
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        D3D11_CREATE_DEVICE_DEBUG,
+        FeatureLevels,
+        sizeof(FeatureLevels) / sizeof(D3D_FEATURE_LEVEL),
+        D3D11_SDK_VERSION,
+        &masterDevice,
+        &pFeatureLevelsOut,
+        &masterContext);
+
+    CComQIPtr<ID3D10Multithread> p_mt(masterContext);
+    p_mt->SetMultithreadProtected(true);
+
+    std::vector<CComPtr<ID3D11Device>> childDevices(2);
+    std::vector<CComPtr<ID3D11DeviceContext>>  child2ndContext(childDevices.size());
+    std::vector <std::thread> threads(childDevices.size());
+
+    for (size_t i = 0; i < childDevices.size(); ++i) {
+        hr = D3D11CreateDevice(nullptr,    // provide real adapter
+            D3D_DRIVER_TYPE_HARDWARE,
+            nullptr,
+            D3D11_CREATE_DEVICE_DEBUG,
+            FeatureLevels,
+            sizeof(FeatureLevels) / sizeof(D3D_FEATURE_LEVEL),
+            D3D11_SDK_VERSION,
+            &childDevices[i],
+            &pFeatureLevelsOut,
+            &child2ndContext[i]);
+
+        CComQIPtr<ID3D10Multithread> _2_mt(child2ndContext[i]);
+        _2_mt->SetMultithreadProtected(true);
+
     }
 
-    for (int k = 0; k < 10;++k) {
 
-        if (initDX12()) {
-            std::cout << "initDX12 failed " << std::endl;
-            return -1;
-        }
+    D3D11_TEXTURE2D_DESC desc = {};
+    desc.Width = 4096;
+    desc.Height = 4096;
+    desc.MipLevels = 1;
+    desc.ArraySize = 1;
+    desc.Format = DXGI_FORMAT_NV12;
+    desc.SampleDesc.Count = 1;
+    desc.Usage = D3D11_USAGE_DEFAULT;
+    desc.BindFlags = D3D11_BIND_DECODER;
+    desc.BindFlags |= D3D11_BIND_SHADER_RESOURCE;
+    desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_NTHANDLE | D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX;
 
-        std::vector<IDirect3DSurface9*> d3d9Surfaces(2);
-        auto hr = service->CreateSurface(
-            16000, 12000,
-            (UINT)(d3d9Surfaces.size() - 1),
-            (D3DFORMAT)MAKEFOURCC('N', 'V', '1', '2'),
-            D3DPOOL_DEFAULT,
-            0,
-            DXVA2_VideoDecoderRenderTarget,
-            d3d9Surfaces.data(),
-            NULL);
-        if (FAILED(hr)) {
-            std::cout << "CreateSurface " << hr << std::endl;
-            return 1;
-        }
+    std::pair<CComPtr<ID3D11Texture2D>, UINT64> texture;
+    hr = masterDevice->CreateTexture2D(&desc, NULL, &texture.first);
 
-        if (copy(d3d9Surfaces[0], d3d9Surfaces[1])) {
-            std::cout << "Copy failed" << std::endl;
-            return -1;
-        }
-
-        commandAllocator = 0;
-        commandList = 0;
-        queue = 0;
-        fence = 0;
-        d3d12 = 0;
-        d3dOn12 = 0;
-
-        for (int i = 0; i < d3d9Surfaces.size(); ++i) {
-            d3d9Surfaces[i]->Release();
-        }
+    CComPtr<IDXGIKeyedMutex> mutex;
+    if (desc.MiscFlags & D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX)
+    {
+        hr = texture.first->QueryInterface(IID_PPV_ARGS(&mutex));
+        hr = mutex->AcquireSync(0, INFINITE);
+        texture.second++;
     }
 
-    std::cout << "PASS" << std::endl;
+    CComPtr<ID3D11Device5> device5;
+    masterDevice->QueryInterface(IID_PPV_ARGS(&device5));
+    std::pair<CComPtr<ID3D11Fence>, UINT64> fence;
+    device5->CreateFence(0, D3D11_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence.first));
+    auto fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+    fence.second = 1;
+
+    desc = {};
+    texture.first->GetDesc(&desc);
+
+    desc.Width = desc.Width;
+    desc.Height = desc.Height;
+    desc.MipLevels = 1;
+    desc.Format = desc.Format;
+    desc.SampleDesc.Count = 1;
+    desc.ArraySize = 1;
+    desc.Usage = D3D11_USAGE_STAGING;
+    desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE | D3D11_CPU_ACCESS_READ;
+    desc.BindFlags = 0;
+    desc.MiscFlags = 0;
+
+    CComPtr<ID3D11Texture2D> staging;
+    hr = masterDevice->CreateTexture2D(&desc, NULL, &staging);
+
+    CComPtr<ID3D11Texture2D> staging1;
+    hr = masterDevice->CreateTexture2D(&desc, NULL, &staging1);
+
+    start = std::chrono::system_clock::now();
+
+    D3D11_MAPPED_SUBRESOURCE lockRect = {};
+    hr = masterContext->Map(staging, 0, D3D11_MAP_WRITE, 0, &lockRect);
+    auto buf = (char*)lockRect.pData;
+    buf = 0x0;
+    masterContext->Unmap(staging, 0);
+
+    masterContext->CopySubresourceRegion(texture.first, 0, 0, 0, 0, staging, 0, NULL);
+    std::chrono::duration<double, std::milli> d = std::chrono::system_clock::now() - start;
+    printf("[%f] master device started WRITE async copy\n", d.count());
+
+    CComPtr<ID3D11DeviceContext4> masterContext4;
+    MFX_CHECK(masterContext->QueryInterface(IID_PPV_ARGS(&masterContext4)));
+    MFX_CHECK(masterContext4->Signal(fence.first, fence.second));
+    MFX_CHECK(fence.first->SetEventOnCompletion(fence.second, fenceEvent));
+    fence.second++;
+
+    for (size_t i = 0; i < threads.size(); ++i)  threads[i] = std::thread{ Routine, (int)i, std::ref(childDevices[i]), std::ref(child2ndContext[i]), std::ref(texture) };
+
+    WaitForSingleObject(fenceEvent, INFINITE);
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f] master device finished WRITE and about to ReleaseSync(%llu)\n", d.count(), texture.second);
+    hr = mutex->ReleaseSync(texture.second);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f] master device started READ copy\n", d.count());
+    masterContext->CopySubresourceRegion(staging1, 0, 0, 0, 0, texture.first, 0, NULL);
+    MFX_CHECK(masterContext4->Signal(fence.first, fence.second));
+    MFX_CHECK(fence.first->SetEventOnCompletion(fence.second, fenceEvent));
+    WaitForSingleObject(fenceEvent, INFINITE);
+    hr = masterContext->Map(staging1, 0, D3D11_MAP_READ, 0, &lockRect);
+    masterContext->Unmap(staging1, 0);
+
+    d = std::chrono::system_clock::now() - start;
+    printf("[%f] master device finished READ copy\n", d.count());
+    
+    for (size_t i = 0; i < threads.size(); ++i) threads[i].join();
+     
     return 0;
 }
